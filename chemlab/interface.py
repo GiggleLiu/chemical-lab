f9c55b42d8db964df1418c8c6956d0b801934135
fzyang <2501705487@qq.com>
2018-01-16 19:47:37 +0800

    frist commit

--- chemlab/interface.py
+++ chemlab/interface.py
@@ -1,12 +1,57 @@
+import argparse
+import json
+import logging
+import os
+from random import shuffle
 
-def chemical_string(structure):
+import numpy as np
+
+MAX_LEN = 120
+PADDING = 'right'
+
+def load_test_data(test_path, n_chars, max_len, char_list, limit=None):
     '''
     structure to string.
     '''
-    pass
+    with open(test_path, 'r') as f:
+        smiles = f.readlines()
+    smiles = [s.strip() for s in smiles]
+    if limit is not None:
+        smiles = smiles[:limit]
+    print('Training set size is', len(smiles))
+    smiles = [smile_convert(i) for i in smiles if smile_convert(i)]
+    print('Training set size is {}, after filtering to max length of {}'.format(len(smiles), max_len))
+    shuffle(smiles)
 
-def generate_samples(num_sample=100):
+    print(('total chars:', n_chars))
+
+    cleaned_data = np.zeros((len(smiles), max_len, n_chars), dtype=np.float32)
+
+    char_lookup = dict((c, i) for i, c in enumerate(char_list))
+
+    for i, smile in enumerate(smiles):
+        for t, char in enumerate(smile):
+            cleaned_data[i, t, char_lookup[char]] = 1
+
+    return cleaned_data,smiles
+    
+def smile_convert(string):
+    if len(string) < MAX_LEN:
+        if PADDING == 'right':
+            return string + " " * (MAX_LEN - len(string))
+        elif PADDING == 'left':
+            return " " * (MAX_LEN - len(string)) + string
+        elif PADDING == 'none':
+            return string
+
+
+def generate_samples(test_data,num_sample=100):
     '''
     generate a batch of data as input.
     '''
-    pass
+    shuffle(test_data)
+    gen_data = test_data[:num_sample]
+    return gen_data
+    
+
+
--- chemlab/test_input.py
+++ chemlab/test_input.py
@@ -1,10 +1,22 @@
 import numpy as np
 from numpy.testing import dec, assert_, assert_raises, assert_almost_equal, assert_allclose
+import json
 
-from .interface import chemical_string, generate_samples
+from interface import load_test_data, generate_samples
+
+MAX_LEN = 120
+limit = 5000
 
 def test_input1():
     num_sample = 100
-    samples = generate_samples(num_sample)
+    char_list = json.load(open("../data/zinc_char_list.json"))
+    n_char = len(char_list)
+    test_data,smiles = load_test_data("../data/250k_rndm_zinc_drugs_clean.smi",n_char,MAX_LEN,char_list,limit)
+    samples = generate_samples(test_data,num_sample)
     assert_(len(samples) == 100)
-    assert_(all([isinstance(s, str) and s!='' for s in samples]))
+    assert_(all([isinstance(s,np.ndarray) and s!='' for s in samples]))
+    
+
+    
+if __name__ == '__main__':
+    test_input1()
